30012b619e660f49c2c4d338e3243662b88958f1
Ryder Cobean <ryderdavid@gmail.com>
2019-05-15 23:12:24 -0400

    got the get_transactions function to work, now moved non function items to scratch file

--- get_transactions.R
+++ get_transactions.R
@@ -1,31 +1,26 @@
-install.packages('tidyverse')
 library(tidyverse)
+library(magrittr)
 library(httr)
 library(jsonlite)
 
-y_api_key <- read_file('.keys/y_api_key') %>% str_trim()
-budget_id <- read_file('.keys/budget_id') %>% str_trim()
-
 y_api_url <- 'https://api.youneedabudget.com/v1'
-since_date <- '2019-05-01'
+y_api_key <- read_file('.keys/y_api_key') %>% str_trim()
 
 
-get_transactions <- function(budget_id, y_api_key, since_date) {
+get_transactions <- function(budget_id, since_date = NULL) {
   
-  require(tidyverse)
-  require(httr)
-  require(jsonlite)
+  yk <- y_api_key
+  d <- since_date
+  b <- budget_id
   
-  request <- GET(url = paste(y_api_url, 'budgets', budget_id, 'transactions', sep = '/'),
-                 query = list(since_date = since_date),
-                 add_headers(Authorization = paste('Bearer', y_api_key, sep = ' ')))
+  request <- GET(url = paste(y_api_url, 'budgets', b, 'transactions', sep = '/'),
+                 query = list(since_date = d),
+                 add_headers(Authorization = paste('Bearer', yk, sep = ' ')))
   
   response <- content(request, as = 'text', encoding = 'UTF-8')
-  df <- fromJSON(response, flatten = TRUE) %>% data.frame()
+  df <- fromJSON(response, flatten = TRUE) %>% data.frame() %>% as_tibble()
   
   return(df)
   
 }
-
-trx <- get_transactions(budget_id = budget_id, y_api_key, since_date = since_date)
 
---
+++ scratch.R
@@ -0,0 +1,38 @@
+source('get_transactions.R')
+
+
+
+budget_id <- read_file('.keys/budget_id') %>% str_trim()
+
+
+
+
+
+trx <- get_transactions(budget_id = budget_id, since_date = '2015-01-01')
+
+trx_for_export <- 
+  trx %>% 
+  filter(data.transactions.flag_color %in% c('red', 'purple')) %>% 
+  transmute(
+    Timestamp = as.Date(data.transactions.date), 
+    Payee = 'Ryder',
+    Amount = data.transactions.amount / 1000 * -1,
+    Purpose = case_when(.$data.transactions.flag_color == 'red' ~ 'for us',
+                        .$data.transactions.flag_color == 'purple' ~ 'for you'),
+    Description = paste(data.transactions.payee_name, data.transactions.memo, sep= ' - ')
+  )
+
+trx_for_export %>% View()
+
+
+plot(trx_for_export$Timestamp, trx_for_export$Amount)
+
+
+trx %>% 
+  filter(grepl('travel', str_to_lower(data.transactions.category_name))) %>% 
+  filter(data.transactions.amount < 0) %>% 
+  mutate(data.transactions.amount = data.transactions.amount / -1000,
+         data.transactions.date = as.Date(data.transactions.date)) -> t
+
+plot(t$data.transactions.date, t$data.transactions.amount, cex = 0.2)  
+  
